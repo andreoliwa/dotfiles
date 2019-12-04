9dd2f12cd50e587432a302ee4215eebfce88a3d4
Augusto Wagner Andreoli <andreoliwa@gmail.com>
2019-12-04 16:14:50 +0100

    feat: use Hammerspoon to arrange windows

---
+++ roles/hammerspoon/files/init.lua
@@ -0,0 +1,35 @@
+-- Documentation: http://www.hammerspoon.org/
+-- Examples:
+-- https://github.com/derekwyatt/dotfiles/blob/master/hammerspoon-init.lua
+-- https://github.com/fikovnik/ShiftIt/wiki/The-Hammerspoon-Alternative
+-- https://github.com/scottwhudson/Lunette
+
+hs.window.animationDuration = 0
+
+-- http://www.hammerspoon.org/docs/hs.application.html#find
+-- To find an application, run this on the console:
+-- hs.application.find('brave')
+
+-- http://www.hammerspoon.org/docs/hs.layout.html
+local laptopScreen = "Color LCD"
+local verticalScreen = "DELL P2414H"
+local horizontalScreen = "HP E241i"
+local windowLayout = {
+    {"Code", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+    {"Brave Browser", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+    {"Slack", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+    {"Vivaldi", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+    {"Skype", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+    {"PyCharm", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+    {"TogglDesktop", nil, horizontalScreen, hs.layout.right30, nil, nil},
+    
+    {"iTerm2", nil, verticalScreen, hs.layout.maximized, nil, nil},
+    {"Zulip", nil, verticalScreen, hs.layout.maximized, nil, nil},
+
+    {nil, hs.window.find('YouTube'), laptopScreen, hs.layout.maximized, nil, nil},
+    {"Spotify", nil, laptopScreen, hs.layout.maximized, nil, nil},
+    {"Telegram", nil, laptopScreen, hs.layout.maximized, nil, nil},
+    {"WhatsApp", nil, laptopScreen, hs.layout.maximized, nil, nil},
+    {"Signal", nil, laptopScreen, hs.layout.maximized, nil, nil},
+}
+hs.layout.apply(windowLayout)

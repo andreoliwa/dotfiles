58196f00fe8d273275446858cc4e2cd6519f9672
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-03-31 17:13:18 +0200

    feat: install Jekyll

--- roles/ruby/activate_rvm.zsh
+++ roles/ruby/activate_rvm.zsh
@@ -1,4 +1,6 @@
 # Lazily load rvm, irb, rake, rails
 
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-group_lazy_load $HOME/.rvm/scripts/rvm rvm irb rake rails
+export PATH="$PATH:$HOME/.rvm/bin:~/.gem/ruby/2.6.0/bin" # Add RVM to PATH for scripting
+
+# This only works with zsh, on the original dotfiles repo
+# group_lazy_load $HOME/.rvm/scripts/rvm rvm irb rake rails

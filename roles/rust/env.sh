f1924d324d7384d449722141906b13f5a3170aef
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-05-28 16:14:33 +0200

    fix: create VSCode dir, don't alias exa

--- roles/rust/env.sh
+++ roles/rust/env.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 source ~/.cargo/env
 
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 
-alias ls=exa
-alias tree='exa --tree'
+# On a new laptop, don't create the alias until exa is installed by Rust
+if [ -n "$(which exa)" ]; then
+    alias ls=exa
+    alias tree='exa --tree'
+fi

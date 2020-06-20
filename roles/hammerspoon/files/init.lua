614192fdfd429f7c7e9666051c26c6701f1307a9
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-06-20 18:11:38 +0200

    fix(hammerspoon): change window layout

--- roles/hammerspoon/files/init.lua
+++ roles/hammerspoon/files/init.lua
@@ -1,3 +1,5 @@
+-- https://www.tutorialspoint.com/lua/
+
 -- Documentation: http://www.hammerspoon.org/
 -- Examples:
 -- https://github.com/derekwyatt/dotfiles/blob/master/hammerspoon-init.lua
@@ -10,32 +12,69 @@ hs.window.animationDuration = 0
 -- To find an application, run this on the console:
 -- hs.application.find('brave')
 
--- http://www.hammerspoon.org/docs/hs.layout.html
-local verticalScreen = "DELL P2414H"
-local horizontalScreen = "HP E241i"
-local laptopScreen = "Color LCD"
-local windowLayout = {
-    -- Left
-    {"iTerm2", nil, verticalScreen, hs.layout.maximized, nil, nil},
-    {"Zulip", nil, verticalScreen, hs.layout.maximized, nil, nil},
+-- http://www.hammerspoon.org/docs/hs.application.html#runningApplications
+for index, app in pairs(hs.application.runningApplications()) do
+    print('App #' .. index .. ': ' .. tostring(app))
+end
 
-    -- Center
-    {"Code", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"Brave Browser", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"Slack", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"Brave Browser Dev", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"Skype", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"PyCharm", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"App Store", nil, horizontalScreen, hs.layout.maximized, nil, nil},
-    {"TogglDesktop", nil, horizontalScreen, hs.layout.right30, nil, nil},
-    {"PhpStorm", nil, horizontalScreen, hs.layout.maximized, nil, nil},
+-- http://www.hammerspoon.org/docs/hs.screen.html#allScreens
+for index, screen in pairs(hs.screen.allScreens()) do
+    print('Screen #' .. index .. ': UUID: ' .. screen:getUUID() .. ' ' .. tostring(screen))
+end
 
-    -- Right
-    {nil, hs.window.find('YouTube'), laptopScreen, hs.layout.maximized, nil, nil},
-    {"Spotify", nil, laptopScreen, hs.layout.maximized, nil, nil},
-    {"Telegram", nil, laptopScreen, hs.layout.maximized, nil, nil},
-    {"WhatsApp", nil, laptopScreen, hs.layout.maximized, nil, nil},
-    {"Signal", nil, laptopScreen, hs.layout.maximized, nil, nil},
-    {"Hammerspoon", "Hammerspoon Console", laptopScreen, hs.layout.maximized, nil, nil},
-}
-hs.layout.apply(windowLayout)
+-- The two external monitors have the same name (HP E241i), so I have to use the UUID instead
+-- If the external monitors are off, fallback to other screens
+local laptop_screen_right = 'Color LCD'
+local horizontal_screen_center = hs.screen.find('565E033B-3870-00DF-A63A-1F5160E08F52') or laptop_screen_right
+local vertical_screen_left = hs.screen.find('7B9820D5-4E5D-A176-973E-790B87B2F4FA') or horizontal_screen_center
+
+-- http://www.hammerspoon.org/docs/hs.geometry.html#rect
+layout_top50 = hs.geometry.rect(0, 0, 1, 0.5)
+layout_bottom50 = hs.geometry.rect(0, 0.5, 1, 0.5)
+
+local window_layout = {}
+
+function config_screen(screen, apps)
+    for index, tuple in pairs(apps) do
+        config = {tuple[1], tuple[2], screen, tuple[3], nil, nil}
+        table.insert(window_layout, config)
+    end
+end
+
+config_screen(vertical_screen_left, {
+    {"iTerm2", nil, hs.layout.maximized},
+    {"Skype", nil, hs.layout.maximized},
+    {"Telegram", nil, layout_top50},
+    {"WhatsApp", nil, layout_bottom50},
+    {"Signal", nil, hs.layout.maximized},
+    {nil, hs.window.find('YouTube'), hs.layout.maximized},
+    {"Preview", nil, hs.layout.maximized},
+})
+
+config_screen(horizontal_screen_center, {
+    {"Finder", nil, layout_top50},
+    {"Code", nil, hs.layout.maximized},
+    {"Brave Browser", nil, hs.layout.maximized},
+    {"Slack", nil, hs.layout.maximized},
+    {"Brave Browser Dev", nil, hs.layout.maximized},
+    {"PyCharm", nil, hs.layout.maximized},
+    {"App Store", nil, hs.layout.maximized},
+    {"TogglDesktop", nil, hs.layout.right30},
+})
+
+config_screen(laptop_screen_right, {
+    {"Spotify", nil, hs.layout.maximized},
+    {"Hammerspoon", "Hammerspoon Console", layout_bottom50},
+})
+
+function apply_window_layout()
+    -- http://www.hammerspoon.org/docs/hs.layout.html
+    hs.layout.apply(window_layout)
+end
+
+apply_window_layout()
+
+-- Apply window layout when a monitor is connected/disconnected
+-- Newsflash: it doesn't work. ;)
+-- http://www.hammerspoon.org/docs/hs.screen.watcher.html
+hs.screen.watcher.new(apply_window_layout)

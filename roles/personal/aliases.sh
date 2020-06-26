301f7a9d9bcd39fcf6c76686fb7874398b3b8a6d
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-06-26 19:40:13 +0200

    fix: rename make alias

--- roles/personal/aliases.sh
+++ roles/personal/aliases.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 alias pc="pycharm-cli"
-alias mks='make -f ~/dotfiles/roles/personal/files/Makefile.sennder'
-alias sg='git-sennder'
+alias sd-make='make -f ~/dotfiles/roles/personal/files/Makefile.sennder'

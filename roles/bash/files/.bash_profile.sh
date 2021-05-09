ac9efec99d49844d3e9a570cd8e15f227622d3bd
Augusto W. Andreoli <andreoliwa@gmail.com>
2021-05-09 19:35:07 +0200

    feat: install asdf
    WIP, still missing more Ansible tasks

--- roles/bash/files/.bash_profile.sh
+++ roles/bash/files/.bash_profile.sh
@@ -3,3 +3,27 @@ if [ -f "$HOME/.bashrc" ]; then
 fi
 
 test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+
+# https://docs.brew.sh/Shell-Completion
+if type brew &>/dev/null; then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+        source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+        for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+            [[ -r "$COMPLETION" ]] && source "$COMPLETION"
+        done
+    fi
+fi
+
+# Where should I install my own local completions?
+# https://github.com/scop/bash-completion/blob/master/README.md#faq
+if [[ -d "$HOME/.local/share/bash-completion/completions" ]]; then
+    for COMPLETION in "$HOME/.local/share/bash-completion/completions/"*; do
+        source "$COMPLETION"
+    done
+fi
+
+# https://github.com/asdf-vm/asdf
+# https://asdf-vm.com/#/core-manage-asdf?id=add-to-your-shell
+source $(brew --prefix asdf)/asdf.sh
--- roles/bash/files/.bashrc.sh
+++ roles/bash/files/.bashrc.sh
@@ -25,7 +25,8 @@ export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # https://github.com/direnv/direnv BEGIN ----------------------------------------------------------------
-eval "$(direnv hook bash)"
+# https://github.com/asdf-community/asdf-direnv#setup
+eval "$(asdf exec direnv hook bash)"
 # https://github.com/direnv/direnv END ----------------------------------------------------------------
 
 # https://github.com/magicmonty/bash-git-prompt BEGIN ---------------------------------------------------
@@ -88,26 +89,6 @@ export HISTIGNORE="$HISTIGNORE:jrnl *"
 
 # Switch from a CLI editor to VSCode in wait mode
 export EDITOR='code --wait'
-
-# https://docs.brew.sh/Shell-Completion
-if type brew &>/dev/null; then
-    HOMEBREW_PREFIX="$(brew --prefix)"
-    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-        source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-    else
-        for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-            [[ -r "$COMPLETION" ]] && source "$COMPLETION"
-        done
-    fi
-fi
-
-# Where should I install my own local completions?
-# https://github.com/scop/bash-completion/blob/master/README.md#faq
-if [[ -d "$HOME/.local/share/bash-completion/completions" ]]; then
-    for COMPLETION in "$HOME/.local/share/bash-completion/completions/"*; do
-        source "$COMPLETION"
-    done
-fi
 
 # https://github.com/junegunn/fzf
 # Taken from 'brew info fzf' (and edited by an ansible role):

632d3c6bcd9cb6a6dac988f53eb93ff2cd145113
W. Augusto Andreoli <andreoliwa@gmail.com>
2021-12-20 03:41:13 +0100

    fix: don't set PROJECT_HOME

--- roles/python_pyenv/env.sh
+++ roles/python_pyenv/env.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-export PROJECT_HOME="$HOME/Code"
-
 # To avoid locale errors in some Python modules.
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
--- roles/python_pyenv/env.xsh
+++ roles/python_pyenv/env.xsh
@@ -1,5 +1,3 @@
-$PROJECT_HOME = str(Path($HOME) / 'Code')
-
 # To avoid locale errors in some Python modules.
 $LC_ALL = "en_US.UTF-8"
 $LANG = "en_US.UTF-8"

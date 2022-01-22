4072b4b95224f0fd96b3ad6ae8759281f4e8d337
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-01-22 02:24:07 +0100

    feat(asdf): add Ruby, NodeJS and yarn plugins

--- roles/node/activate_nvm.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-# Lazily load nvm so that it doesn't slow down shell startup time
-# https://github.com/creationix/nvm/issues/782
-export NVM_DIR="$HOME/.nvm"
-# TODO not working on bash
-# group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx yarn
--- roles/ruby/activate_rvm.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# Lazily load rvm, irb, rake, rails
-
-export PATH="$PATH:$HOME/.rvm/bin:$HOME/.gem/ruby/2.6.0/bin" # Add RVM to PATH for scripting
-
-# This only works with zsh, on the original dotfiles repo
-# group_lazy_load $HOME/.rvm/scripts/rvm rvm irb rake rails

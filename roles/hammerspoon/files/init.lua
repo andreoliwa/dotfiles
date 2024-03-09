fa8136fe6b40e63d8e474f2644fdacceab1573d8
W. Augusto Andreoli <andreoliwa@sent.com>
2024-03-09 23:45:40 +0100

    fix: Signal on big screen

--- roles/hammerspoon/files/init.lua
+++ roles/hammerspoon/files/init.lua
@@ -234,7 +234,7 @@ config_app('PyCharm', '', nil, {{wide_screen, true, hs.layout.right50}, {horizon
 config_app('RustRover-EAP', '', nil, {{wide_screen, true, hs.layout.right50}, {horizontal_screen, true, hs.layout.right70}})
 config_app('ScanSnap Home', hs.window.find('- Scan'), nil, {{laptop_screen, true, hs.layout.right70}})
 config_app('ScanSnap Home', nil, nil, {{laptop_screen, true, hs.geometry.rect(0.42, 0.23, 0.5, 0.5)}})
-config_app('Signal', nil, hide_when_working, {{laptop_screen, true, hs.layout.maximized}})
+config_app('Signal', nil, hide_when_working, {{wide_screen, not at_the_office, hs.layout.left50}, {horizontal_screen, not at_the_office, hs.layout.left70}})
 config_app('Skype', nil, nil, {{laptop_screen, true, hs.layout.maximized}})
 config_app('Slack', '', nil, {{wide_screen, true, hs.layout.left50}, {horizontal_screen, true, hs.layout.left70}})
 config_app('Speedtest', nil, nil, {{laptop_screen, true, hs.layout.left50}})

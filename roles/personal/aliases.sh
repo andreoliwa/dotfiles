347ea3620c322163e7a8d62a3fb7bca4b73eac5b
W. Augusto Andreoli <andreoliwa@sent.com>
2025-07-27 10:24:05 +0200

    feat: alias to query done and canceled tasks

--- roles/personal/aliases.sh
+++ roles/personal/aliases.sh
@@ -2,4 +2,5 @@
 alias pc="pycharm-cli"
 alias sd-make='make -f ~/Code/toolbox/Makefile.sennder'
 alias ft='invoke logseq.find-task'
+alias ftc='invoke logseq.find-task --completed'
 alias fq='invoke logseq.rg'

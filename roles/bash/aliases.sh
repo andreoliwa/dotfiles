78b1286f1e914ce93533c36e6a406939f6858e8a
W. Augusto Andreoli <andreoliwa@sent.com>
2025-09-10 01:11:54 +0200

    fix: rsync from macOS converting UTF-8 properly

--- roles/bash/aliases.sh
+++ roles/bash/aliases.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-alias rsync-default="rsync --human-readable --recursive --times --from0 --verbose --compress --progress --modify-window=1"
+alias rsync-default="rsync --human-readable --recursive --times --from0 --verbose --compress --progress --modify-window=1 --iconv=UTF-8-MAC,UTF-8"
 
 # https://stackoverflow.com/questions/17998978/removing-colors-from-output
 alias decolorize='sed -r "s/\\x1B\\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"'

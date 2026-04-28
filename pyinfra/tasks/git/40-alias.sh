89a2fd9eacfdd575ae21f7548579d9327b820d16
W. Augusto Andreoli <andreoliwa@sent.com>
2026-04-28 15:56:11 +0200

    fix(git): alias to show untracked files

--- pyinfra/tasks/git/40-alias.sh
+++ pyinfra/tasks/git/40-alias.sh
@@ -4,5 +4,8 @@ alias gl="git pull"
 alias gs='git status'
 alias gsta="git add -A && git stash push"
 alias gstp='git stash pop'
+# -u forces listing of all untracked files, overriding repos (e.g. Pedregal) that set
+# status.showUntrackedFiles=no for performance; otherwise stray files stay hidden.
+alias gsu='git status -u'
 alias gwl='git worktree list'
 # keep-sorted end

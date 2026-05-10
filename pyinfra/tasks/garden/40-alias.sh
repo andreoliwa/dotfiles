ef1e3f9769577ca4fc8231f2353099bf564bf972
W. Augusto Andreoli <andreoliwa@sent.com>
2026-05-10 15:42:09 +0200

    feat(garden): add alias to the install command

--- pyinfra/tasks/garden/40-alias.sh
+++ pyinfra/tasks/garden/40-alias.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # https://github.com/garden-rs/garden
+# keep-sorted start
 alias ga='garden'
+# Install with verbosity by default, to show the underlying command being executed
+alias gai='garden install -vv'
 alias gi='garden ide'
 alias ide='garden ide'
+# keep-sorted end

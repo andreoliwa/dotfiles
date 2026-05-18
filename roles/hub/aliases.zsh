967d8bd0aa07b95155ac39a987ff7a351b26153c
W. Augusto Andreoli <andreoliwa@sent.com>
2026-05-18 02:53:00 +0200

    chore: drop unused Ansible roles and playbook refs

--- roles/hub/aliases.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-# Add git aliases for hub
-eval "$(hub alias -s)"
--- roles/personal/aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-alias pc="pycharm-cli"
-alias sd-make='make -f ~/Code/toolbox/Makefile.sennder'
-alias ft='invoke logseq.find-task'
-alias ftc='invoke logseq.find-task --completed'
-alias fq='invoke logseq.rg'

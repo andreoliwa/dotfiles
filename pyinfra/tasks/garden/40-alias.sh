6b65c661d8957fb6b9db38f9b943164583fb1985
W. Augusto Andreoli <andreoliwa@sent.com>
2026-05-22 00:31:43 +0200

    chore(garden): add -vv to ide aliases, fix comment sort order

--- pyinfra/tasks/garden/40-alias.sh
+++ pyinfra/tasks/garden/40-alias.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # https://github.com/garden-rs/garden
 # keep-sorted start
-alias ga='garden'
 # Install with verbosity by default, to show the underlying command being executed
+alias ga='garden'
 alias gai='garden install -vv'
-alias gi='garden ide'
-alias ide='garden ide'
+alias gi='garden ide -vv'
+alias ide='garden ide -vv'
 # keep-sorted end

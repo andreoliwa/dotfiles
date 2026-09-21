e9cf2e4dd646d82a4ff95ba10927f44e78e814e7
W. Augusto Andreoli <andreoliwa@sent.com>
2026-09-21 22:02:03 +0200

    fix(rsync): correct alias for Linux

--- pyinfra/tasks/bash/20-alias.sh
+++ pyinfra/tasks/bash/20-alias.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-alias rsync-default="rsync --human-readable --recursive --times --from0 --verbose --compress --progress --modify-window=1 --iconv=UTF-8-MAC,UTF-8"
+if [[ "$(uname)" == "Darwin" ]]; then
+    alias rsync-default="rsync --human-readable --recursive --times --from0 --verbose --compress --progress --modify-window=1 --iconv=UTF-8-MAC,UTF-8"
+else
+    alias rsync-default="rsync --human-readable --recursive --times --from0 --verbose --compress --progress --modify-window=1"
+fi
 
 # https://stackoverflow.com/questions/17998978/removing-colors-from-output
 alias decolorize='sed -r "s/\\x1B\\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"'
